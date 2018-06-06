c1e9cc66ea568e9eabb31af8d8ac2d98e3d3c40b
Colin Bates and James Hanley <james@>
2018-06-06 15:30:39 -0700

    finish up to vowel check

--- pig-latin/js/scripts.js
+++ pig-latin/js/scripts.js
@@ -1,10 +1,11 @@
 //Front-end
 $(document).ready(function(){
   $("#pigLatinForm").submit(function(event){
-    debugger;
+    // debugger;
     event.preventDefault();
     //var splitPig = [];
     var inputSentance = $("input#sentenceInput").val();
+    console.log(inputSentance);
     //if (inputPigLatin.length > 0){
     translateWords(inputSentance);
     //  inputPigLatin.split('');
@@ -18,23 +19,46 @@ $(document).ready(function(){
   //  splitPig(inputPigLatin);
   //  foreach()
   });
-});
+
+
 //Back-end
 
 //Function to split each word, call vowelCheck function on originalSentance for each step
 function translateWords(sentance) {
   var originalSentance = sentance.split(" ");
-  for(index = 0; index < orignialSentance; index++){
-    originalSentance[index] = vowelCheck(originalSentence)
+  console.log(originalSentance);
+
+  for(index = 0; index < originalSentance.length; index++){
+    originalSentance[index] = wordSplit(originalSentance[index]);
   }
-  var joinOriginalSentance = originalSentance.join(" ");
-  return joinOriginalSentance
+  console.log(originalSentance);
 }
 
-function vowelCheck(word){
+function wordSplit(word) {
+  console.log(word);
+  var splitWord = word.split('');
+
+  console.log(splitWord);
+  forEach()(splitWord))
+}
+function vowelCheck(vowel){
+(['a', 'e', 'i', 'o', 'u'])
+};
+//function vowelCheck(word){
   //
-  if()
-}
-function listVowels(letter){
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
-}
+  //if(//single letter word begins with vowel, add ay){
+
+  //}else if {
+    //starts with vowel
+//  } else {
+    //return without mutations
+//  }
+//}
+//function listVowels(letter){
+//  var vowels = ;
+//  var wordLength = letter.length();
+//  var splitLetters = letter.split("");
+  //forEach(splitLetters(function(word)){
+
+//  }
+});
