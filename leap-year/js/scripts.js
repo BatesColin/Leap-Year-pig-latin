bcd237bc1f1de33a5b3c7ec608416a56f70b2f5d
Colin Bates and James Hanley <james@>
2018-06-06 09:32:58 -0700

    Finished all required specs for leap year detector

--- leap-year/js/scripts.js
+++ leap-year/js/scripts.js
@@ -1,13 +1,10 @@
 //Back-end
 var leapYear = function(year) {
-  if (year % 4 === 0){
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
     return true;
-  } else if (year % 100 === 0) {
-    return false;
   } else {
     return false;
   }
-
 }
 
 //Front End
